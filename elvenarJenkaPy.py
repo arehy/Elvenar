ea0f42ab23e3821709f2c8d0715d3c2e8cc8f696
arehy <jeneiimre@hotmail.com>
2020-01-14 15:52:52 +0100

    elvenarJenkaPy.py bekerült

---
+++ elvenarJenkaPy.py
@@ -0,0 +1,17 @@
+#import pyautogui
+import time
+
+""" def rSleep(min, max):
+    r = randint(min, max)
+    time.sleep(r) """
+
+print('1')
+time.sleep(randint(2, 10))
+print('2')
+time.sleep(randint(2, 10))
+print('3')
+time.sleep(randint(2, 10))
+print('4')
+time.sleep(randint(2, 10))
+print('5')
+time.sleep(randint(2, 10))
