8ab28e6cfc7fce26ddb67776b2eed74dce81c80b
arehy <jeneiimre@hotmail.com>
2020-01-16 15:47:19 +0100

    coordCalc.py működik

---
+++ coordCalc.py
@@ -0,0 +1,74 @@
+from tkinter import*
+from pyautogui import position
+from time import sleep
+from keyboard import is_pressed
+
+window = Tk()
+window.geometry("130x170")
+ins = Text(window, font=("Helvetica",12), borderwidth=2, relief="solid")
+
+ent = Entry(window)
+ent.pack()
+ent.delete(0, END)
+
+
+
+coord1 = Label(window, text='ctrl+shift+1')
+coord1.pack()
+coord2 = Label(window, text='ctrl+shift+1')
+coord2.pack()
+coordKivonva = Label(window, text='ctrl+shift+n')
+coordKivonva.pack()
+coordKivonvaOsztva = Label(window, text='ctrl+shift+n')
+coordKivonvaOsztva.pack()
+x1 = x2 = y1 = y2 = 0
+
+
+while True:
+    x, y = position()
+    posStrx = str(x).rjust(4) #az rjust (rjust(4, '0')) a stringet átalakítja annyi karakteresre, ami a zárójelben az első szám, és azzal pótolja, ami a 2. érték
+    posStry = str(y).rjust(4)
+    mousepos = 'X: '+posStrx+' Y: '+posStry
+    mouseposFix1 = 'ctrl+shift+1: '+posStrx+', '+posStry
+    mouseposFix2 = 'ctrl+shift+2: '+posStrx+', '+posStry
+    if is_pressed('ctrl+shift+1'):
+        x1 = x
+        y1 = y
+        coord1.configure(text=mouseposFix1)
+    if is_pressed('ctrl+shift+2'):
+        x2 = x
+        y2 = y
+        coord2.configure(text=mouseposFix2)
+    ins.insert(INSERT, mousepos)
+    ins.pack()
+    sleep(0.0005)
+
+    if is_pressed('ctrl+shift+n'):
+        try:
+            epuletSzam = ent.get()
+            epuletSzam = int(epuletSzam)-1
+        except ValueError:
+            epuletSzam = 1
+        if epuletSzam == 0:
+            xKivonva = yKivonva = xKivonvaOsztva = yKivonvaOsztva = '0'
+        else:
+            xKivonva = str(x1-x2)
+            yKivonva = str(y1-y2)
+            xKivonvaOsztva = str(round((x1-x2)/epuletSzam))
+            yKivonvaOsztva = str(round((y1-y2)/epuletSzam))
+        coordKivonva.configure(text='ctrl+shift+n: '+xKivonva+', '+yKivonva)
+        coordKivonvaOsztva.configure(text='ctrl+shift+n: '+xKivonvaOsztva+', '+yKivonvaOsztva)
+        
+    if is_pressed('ctrl+shift+b'):
+        break
+
+
+
+    window.update()
+    ins.delete(1.0,END)
+    ins.pack()
+
+        
+        
+
+window.destroy()
--- mousePosvishnurvpModify.py
+++ mousePosvishnurvpModify.py
@@ -4,30 +4,53 @@ from time import sleep
 from keyboard import is_pressed
 
 window = Tk()
-w = 200
-h = 40
-a = 1
-ws = window.winfo_screenwidth()
-hs = window.winfo_screenheight()
-window.wm_attributes('-topmost','true')
-text = Text(window, font=("Helvetica",20), borderwidth=2, relief="solid")
-p=1
-w = Label(window, text='0')
-w.pack()
-w2 = Label(window, text='0')
-w2.pack()
+window.geometry("130x170")
+ins = Text(window, font=("Helvetica",12), borderwidth=2, relief="solid")
+
+ent = Entry(window)
+ent.pack()
+ent.delete(0, END)
+
+
+
+coord1 = Label(window, text='0')
+coord1.pack()
+coord2 = Label(window, text='0')
+coord2.pack()
+coordKivonva = Label(window, text='0')
+coordKivonva.pack()
+coordKivonvaOsztva = Label(window, text='0')
+coordKivonvaOsztva.pack()
+
+
 while True:
     x, y = position()
-    posStrx = str(x).rjust(4)
+    posStrx = str(x).rjust(4) #az rjust (rjust(4, '0')) a stringet átalakítja annyi karakteresre, ami a zárójelben az első szám, és azzal pótolja, ami a 2. érték
     posStry = str(y).rjust(4)
     mousepos = 'X: '+posStrx+' Y: '+posStry
-    if is_pressed('ctrl+shift+x'):
-        w.configure(text=mousepos)
-    if is_pressed('ctrl+shift+y'):
-        w2.configure(text=mousepos)
-    text.insert(INSERT, mousepos)
-    text.pack()
+    if is_pressed('ctrl+shift+1'):
+        x1 = x
+        y1 = y
+        coord1.configure(text=mousepos)
+    if is_pressed('ctrl+shift+2'):
+        x2 = x
+        y2 = y
+        coord2.configure(text=mousepos)
+    ins.insert(INSERT, mousepos)
+    ins.pack()
     sleep(0.0005)
+
+    if is_pressed('ctrl+shift+n'):
+    
+        epuletSzam = ent.get()
+        epuletSzam = int(epuletSzam)-1
+        xKivonva = str(x1-x2)
+        yKivonva = str(y1-y2)
+        xKivonvaOsztva = str((x1-x2)/epuletSzam)
+        yKivonvaOsztva = str((y1-y2)/epuletSzam)
+
+        coordKivonva.configure(text=xKivonva+', '+yKivonva)
+        coordKivonvaOsztva.configure(text=xKivonvaOsztva+', '+yKivonvaOsztva)
 
     if is_pressed('ctrl+shift+b'):
         break
@@ -35,8 +58,8 @@ while True:
 
 
     window.update()
-    text.delete(1.0,END)
-    text.pack()
+    ins.delete(1.0,END)
+    ins.pack()
 
         
         
